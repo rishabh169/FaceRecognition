481c888adf37519d43a4a643c043588ef350a887
rishabh Yadav <rishabhy6699@gmail.com>
2020-05-14 23:03:54 +0530

    Updates

---
+++ precache-manifest.9f75bfd62548b091cad556939a8412d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2a35975d57a7eb3144365e4749d68c",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "1b0be261d6cba00e5feb",
+    "url": "/FaceRecognition/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "38c74e9a5d9c78323aca",
+    "url": "/FaceRecognition/static/css/main.5f9dbafb.chunk.css"
+  },
+  {
+    "revision": "1b0be261d6cba00e5feb",
+    "url": "/FaceRecognition/static/js/2.7de42621.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/FaceRecognition/static/js/2.7de42621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c74e9a5d9c78323aca",
+    "url": "/FaceRecognition/static/js/main.6856bf37.chunk.js"
+  },
+  {
+    "revision": "7457395595fdc0e44a7c",
+    "url": "/FaceRecognition/static/js/runtime-main.9aeae4c2.js"
+  }
+]);
