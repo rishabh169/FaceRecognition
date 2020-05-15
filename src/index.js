6ac30f3eb87e44ca55403986965736fac8bc4520
rishabh Yadav <rishabhy6699@gmail.com>
2020-05-15 07:50:37 +0530

    public file deleted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'tachyons';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
     <App />,
